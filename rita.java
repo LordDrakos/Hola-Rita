481d2144b99ceb796ce06f37fd7dabdd100152e2
Antonio Djigo <drakosdna@gmail.com>
2017-01-30 13:40:44 +0000

    Update rita.java

--- rita.java
+++ rita.java
@@ -1,7 +1,10 @@
-
+  
 public class Main{
 
     public static void main(String[] args) {
               System.out.println("Hola Rita");
-              }
+            for (int i = 0; i < 3; i++){
+                System.out.println("Frase nº" +i);
+            }
+    }
      }
